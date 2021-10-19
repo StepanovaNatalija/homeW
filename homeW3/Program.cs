6322f152fb9f6edc0d1e7ed69b83b9e1e9cfb0b8
StepanovaNatalija <stepanova@natalie@gmail.com>
2021-10-19 23:15:25 +0300

    19okt23:15

---
+++ homeW3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace homeW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number;
+            Console.Write("Enter the number to find its absolute value: ");
+            number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Absolute value : " + GetAbsoluteValue(number));
+        }
+
+    static double GetAbsoluteValue(double number)
+        {
+            if (number < 0)
+                number =number * -1;
+
+            return number;
+        }
+
+    }
+}
