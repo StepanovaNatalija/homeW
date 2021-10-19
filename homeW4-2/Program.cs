1532a94cbff2a272ad8c7a553f8022132bd781f7
StepanovaNatalija <stepanova@natalie@gmail.com>
2021-10-20 00:02:21 +0300

    20okt00:01

---
+++ homeW4-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace homeW4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            Console.Write("This program calculates an area of a circle. Enter radius: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            Console.WriteLine("An area of a circle is : " + GetAreaCircle(radius));
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            double sideLength;
+            Console.Write("This program calculates an area of a regular square. Enter side leghth: ");
+            sideLength = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            Console.WriteLine("An area of a square is : " + GetAreaSquare(sideLength));
+        }
+
+        static double GetAreaCircle(double radius)
+        {
+            double areaCircle;
+            double pi = Math.PI;
+            areaCircle = pi * radius * radius;
+            return areaCircle;
+        }
+
+        static double GetAreaSquare(double sideLength)
+        {
+            double areaSquare;
+            areaSquare = sideLength * sideLength;
+            return areaSquare;
+        }
+
+    }
+}
