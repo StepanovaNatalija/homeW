cdbc4afb306b32a9e07742555e9e01a3d24ef65a
StepanovaNatalija <stepanova@natalie@gmail.com>
2021-10-19 23:08:19 +0300

    19okt23:07

---
+++ homeW1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace homeW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Homework1
+
+            //Console.Write("My name is Natalija\nI am from Latvia\nI learn C#\nYou too?\nCongratulations!");
+            //Console.WriteLine(" ");
+            //Console.ReadKey();
+
+            #endregion
+
+
+            #region Homework2
+
+            //int myAge = 48;               // integer (whole number)
+            //double myDouble = myAge;  // automatic casting: int to double
+            //float myHight = 1.72F;  // floating point number
+            //char myNameFirstLetter = 'N';         // character
+            //bool truth = true;          // boolean
+            //string myName = "Natalija";     // string
+            //string color = "green";  //string 
+            //string drink = "tea";  //string 
+            //string liquid = $"My favorite drink is: {color} {drink}"; //  string interpolation
+            //Console.WriteLine("Hello! My name is " + myName + ", but everybody calls me \"" + myNameFirstLetter + "\"");
+            //Console.WriteLine("I am " + myDouble + " years old and " + myHight + " tall");
+            //Console.WriteLine(liquid);
+            //Console.WriteLine("All this is " + truth + ".");
+
+            #endregion
+
+
+            
+        }
+    }
+}
