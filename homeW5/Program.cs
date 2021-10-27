85d73b2c2efec10ff7c4f44b46a06aafd65f3dbe
StepanovaNatalija <stepanova@natalie@gmail.com>
2021-10-27 23:54:43 +0300

    27okt23:54

---
+++ homeW5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using static System.String;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace homeW5
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(b[A-Z]\w*\s*)=$";
+            string text = "Use Of Technology";
+            MatchCollection matches;
+
+            Regex defaultRegex = new Regex(pattern);
+            // Get matches of pattern in text
+            matches = defaultRegex.Matches(text);
+            Console.WriteLine("Checking whether all words start with capital letters in: " + text);
+            if (defaultRegex.IsMatch(text))
+            { Console.WriteLine(matches); }
+        }
+    }
+}
