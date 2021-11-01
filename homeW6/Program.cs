38e5fff13ab4798ca0e828d3bbdffeae7e1b3026
StepanovaNatalija <stepanova@natalie@gmail.com>
2021-11-01 21:33:12 +0200

    01nov21

--- homeW6/Program.cs
+++ homeW6/Program.cs
@@ -6,10 +6,19 @@ namespace homeW6
     {
         static void Main(string[] args)
         {
-            
+            int[] myArray1 = { 12, 223, 3343, 45345, 23423, 23, 1112, 456, 33, 1 };
+            Console.Write("This program assignes the same values from one array to another: ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("myArray1 is: 12, 223, 3343, 45345, 23423, 23, 1112, 456, 33, 1  ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("myArray2 is: " + myArray2[0] + ", " + myArray2[1] + ", " + myArray2[2] + ", " + myArray2[3] + ", " + myArray2[4] + ", " + myArray2[5] + ", " + myArray2[6] + ", " + myArray2[7] + ", " + myArray2[8] + ", " + myArray2[9]);
+            Console.WriteLine();
+            Console.WriteLine();
 
 
         }
-        
+
     }
 }
