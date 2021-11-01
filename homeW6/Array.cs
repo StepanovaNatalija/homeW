b31bde0c30587ec70758e5064516e343c7b390eb
StepanovaNatalija <stepanova@natalie@gmail.com>
2021-11-01 21:34:08 +0200

    01nov21

---
+++ homeW6/Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeW6
+{
+    public class Array
+    {
+        public int[] myArray2;
+
+    public Array(int[] array)
+        {
+            int[] myArray1 = new int[10];
+            int[] myArray2 = new int[10];
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                myArray2[i] = myArray1[i];
+            }
+            myArray2 = array;
+        }
+    } 
+}
