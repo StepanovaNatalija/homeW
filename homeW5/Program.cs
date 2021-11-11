8e2cc7ab9bac2ab0fe358cc4d502fcd78dbfc0c8
StepanovaNatalija <92057927+StepanovaNatalija@users.noreply.github.com>
2021-11-11 16:08:32 +0200

    Update Program.cs

--- homeW5/Program.cs
+++ homeW5/Program.cs
@@ -8,12 +8,37 @@ using System.Threading.Tasks;
 
 
 namespace homeW5
-{
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
+{  
+    class Program  
+    {  
+        static void Main(string[] args)  
+        {  
+            Regex r = new Regex(@"^\+\d\d\-\d\d\d\-\d\d\d\-\d\d\d");  
+            string[] str = { "+35-392-022-194", "+958 28492-503" };  
+            foreach (string s in str)  
+            {  
+                Console.WriteLine("{0} {1} a valid mobile number.", s,  
+                r.IsMatch(s) ? "is" : "is not");  
+            }  
+       
+ 
+            string givenString = "Use Of Technology";  
+           
+            string[] str = Regex.Split(givenString, @"\s+");
+           
+            Regex r = new Regex(@"\A[A-Z]");
+           
+            foreach (string s in str)
+            {
+                if (r.IsMatch(s))
+           
+            Console.WriteLine("True");
+           
+                else
+            Console.WriteLine("False");
+            }
+        
+            // Alternative
             string pattern = @"(b[A-Z]\w*\s*)=$";
             string text = "Use Of Technology";
             MatchCollection matches;
