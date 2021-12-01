c8dd3a92413b71c77267ffb8a8fe84ce0d0e5785
StepanovaNatalija <stepanova@natalie@gmail.com>
2021-12-01 21:26:04 +0200

    01.12

---
+++ EntityFramework/Context/UniversityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework
+{
+    public class UniversityDbContext : DbContext
+    {
+        public DbSet<Students> Students { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer(@"Server=LAPTOP-20BVNTSH\SQLEXPRESS; Database=UniversityDB; Trusted_Connection=True;");
+    }
+}
---
+++ EntityFramework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+
+namespace EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Students>();
+            using (var context = new UniversityDbContext())
+            {
+                students = context.Students.ToList();
+            }
+            students.ForEach(x => Console.WriteLine(x.Name));
+            Console.ReadLine();
+        }
+        
+    }
+}
